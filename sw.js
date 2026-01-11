3b71b6c5a76c0f9219a6be9dc70441d6e9b1befe
projeto-amigos-2026 <artur.pdlima@gmail.com>
2026-01-11 08:42:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,8 @@
-// Service Worker para permitir a instalação como APP
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Instalado');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Necessário para o navegador permitir a instalação
+  e.respondWith(fetch(e.request));
+});
