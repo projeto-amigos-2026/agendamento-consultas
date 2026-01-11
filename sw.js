5ab07065a519a3db1484d616c1c1a27ba719894f
projeto-amigos-2026 <artur.pdlima@gmail.com>
2026-01-11 08:55:45 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 self.addEventListener('install', (e) => {
-  console.log('Service Worker Instalado');
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
-  // Necessário para o navegador permitir a instalação
   e.respondWith(fetch(e.request));
 });
